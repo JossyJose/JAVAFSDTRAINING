c6d23682314bf7078350961e5f15a74f2a81d668
JossyJose <87644962+JossyJose@users.noreply.github.com>
2021-08-10 18:24:25 +0530

    Add files via upload

---
+++ SpringBootExample_Project/HelloController.java
@@ -0,0 +1,14 @@
+package com.ust.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/")
+	public String hello()
+	{
+		return "This is java training";
+	}
+
+}
---
+++ SpringBootExample_Project/SpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.ust.example;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+
+	public static void main(String args[])
+	{
+		SpringApplication.run(SpringBootApplication.class, args);
+	}
+}
